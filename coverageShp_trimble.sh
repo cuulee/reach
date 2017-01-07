2b791a7d923003dabf53247904b85d8cce8c8405
ANCELIN Julien <julien.ancelin@inra.fr>
2017-01-07 18:45:25 +0100

    Update coverageShp_trimble.sh

--- coverageShp_trimble.sh
+++ coverageShp_trimble.sh
@@ -1,6 +1,7 @@
 
 
-
+## extract data from EZguide500 Trimble 
+## name Coverage.shp to 1.shp, 2.shp,...
 find /home/jancelin/Bureau/Default_Farm/ -name "Coverage.shp" | cat -n | while read n f; do mv "$f" "/home/jancelin/Bureau/mnt_compterre/$n.shp"; done
 find /home/jancelin/Bureau/Default_Farm/ -name "Coverage.shx" | cat -n | while read n f; do mv "$f" "/home/jancelin/Bureau/mnt_compterre/$n.shx"; done
 find /home/jancelin/Bureau/Default_Farm/ -name "Coverage.dbf" | cat -n | while read n f; do mv "$f" "/home/jancelin/Bureau/mnt_compterre/$n.dbf"; done
